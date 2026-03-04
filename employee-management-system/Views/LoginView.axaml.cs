b92b1ec357d47b1c971585b9ff6577ab29739efd
krystian-bombola <krystian.bombola@icloud.com>
2026-03-04 20:19:57 +0100

    feat(auth): add login panel

---
+++ employee-management-system/Views/LoginView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace employee_management_system.Views;
+
+public partial class LoginView : UserControl
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+}
